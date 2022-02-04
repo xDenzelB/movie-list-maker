3dacb197552cd2c4a6a3eb4968c6830f8e3b2427
Denzel <dbartolaba123@gmail.com>
2022-02-03 16:46:33 -0800

    Added in movieList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState(null);
+  const [movieFormYear, setMovieFormYear] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('');
+  
+  function addMovie(newMovie) {
+    const updateMovies = [...allMovies, newMovie];
+
+    setAllMovies(updateMovies);
+  }
+
+  function deleteMovie(title) {
+    const index = allMovies.findIndex(allMovies => movie.title === title);
+
+    allMovies.splice(index, 1);
+
+    setAllMovies([...allMovies]);
+  }
   return (
     <div className="App">
     
---
+++ src/MovieForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MovieForm({ allMovies, deleteMovie }) {
+  return <div></div>;
+}
